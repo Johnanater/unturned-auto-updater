d0716b7ca6d737af40373edc2861b0afd2b5b784
Johnanater <10360610+Johnanater@users.noreply.github.com>
2018-12-15 18:29:22 -0800

    Remove extra +app_info_print
    Why was that even there...

--- autoupdate.bat
+++ autoupdate.bat
@@ -55,7 +55,7 @@ if not exist temp\serverver.txt (
 ::The appcache has to be deleted in order for SteamCMD to actually update the app info
 ::Yes... I used sed and wget... (Sed is win-bash and wget is mingw32)
 if exist "lib\SteamCMD\appcache\appinfo.vdf" (del /f lib\SteamCMD\appcache\appinfo.vdf)
-lib\SteamCMD\steamcmd.exe +login %SteamUser% %SteamPass% +app_info_update 1 +app_info_print "304930" +app_info_print "304930" +quit >temp\ver.txt
+lib\SteamCMD\steamcmd.exe +login %SteamUser% %SteamPass% +app_info_update 1 +app_info_print "304930" +quit >temp\ver.txt
 findstr /r "buildid" temp\ver.txt > temp\vers.txt
 lib\sed.exe 2,9d temp\vers.txt > temp\version.txt
 
